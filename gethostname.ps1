c93b4608ce3c59c3e14c76a2d55467fd2bcf21f5
Archerino <52434477+Archerino@users.noreply.github.com>
2019-07-02 10:26:20 +0800

    Update gethostname.ps1

--- gethostname.ps1
+++ gethostname.ps1
@@ -1,9 +1,16 @@
 ﻿$ipaddress=Get-Content -Path D:\ip.txt | Where-Object {$_.Trim() -ne ''}
 $csv = @()
+
 ForEach ($i in $ipaddress){ 
-$value=new-object psobject
-$value | Add-Member -MemberType NoteProperty -Name IP -Value ([String]$i)
-$value | Add-Member -MemberType NoteProperty -Name Hostname -Value ([System.Net.Dns]::GetHostByAddress($i).HostName)
-$csv +=$value
+  try{
+    $value=new-object psobject
+    $value | Add-Member -MemberType NoteProperty -Name IP -Value ($i)
+    $value | Add-Member -MemberType NoteProperty -Name Hostname -Value ([System.Net.Dns]::GetHostByAddress($i).HostName)
+    $csv +=$value
+    }
+    catch{
+      Write-Host "Unable to obtain hostname from:"$i
+    }
 }
+
 $csv | Export-Csv D:\hostnames.csv
