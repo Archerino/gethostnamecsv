aab09b0c2ff37d5b079760abf77091280a42345b
Archerino <52434477+Archerino@users.noreply.github.com>
2019-07-02 10:07:47 +0800

    Add files via upload

---
+++ gethostname.ps1
@@ -0,0 +1,9 @@
+﻿$ipaddress=Get-Content -Path D:\ip.txt | Where-Object {$_.Trim() -ne ''}
+$csv = @()
+ForEach ($i in $ipaddress){ 
+$value=new-object psobject
+$value | Add-Member -MemberType NoteProperty -Name IP -Value ([String]$i)
+$value | Add-Member -MemberType NoteProperty -Name Hostname -Value ([System.Net.Dns]::GetHostByAddress($i).HostName)
+$csv +=$value
+}
+$csv | Export-Csv D:\hostnames.csv
